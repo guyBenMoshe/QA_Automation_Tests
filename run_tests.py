7fce1fe14de8e95c4a144d539095c1635be49f6c
guy Ben moshe <guybenmoshe@h-MacBook-Air-sl-guy.local>
2025-02-12 16:25:29 +0200

    הוספת דוח בדיקות אוטומטי

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 import pytest
 
 if __name__ == "__main__":
-    exit_code = pytest.main(["-v", "--disable-warnings"])
+    exit_code = pytest.main(["-v", "--disable-warnings", "--html=report.html"])
     exit(exit_code)
